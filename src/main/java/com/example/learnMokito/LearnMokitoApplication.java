02cafbe7622af577ed284e28744ed6035e04f3ed
Shangxing Jiang <jsx2017@outlook.com>
2019-11-12 15:21:52 -0500

    .

--- src/main/java/com/example/learnMokito/LearnMokitoApplication.java
+++ src/main/java/com/example/learnMokito/LearnMokitoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LearnMokitoApplication {
-
+	// add a comment
 	public static void main(String[] args) {
 		SpringApplication.run(LearnMokitoApplication.class, args);
 	}
